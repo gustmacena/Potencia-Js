71efb80c0fd539f9026cd5a3b2e5ce09a28727e7
gustmacena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 21:12:10 -0300

    Potencia

---
+++ potencia.js
@@ -0,0 +1,11 @@
+let base = 2
+let expoente = 5  
+let res = base
+
+for(let i=2; i<=expoente; i++){
+    
+    res = res * base
+    
+    
+}    
+    console.log(res.toFixed(2)) //.toFixed(2) - para acrescentar duas casas decimais na frente
